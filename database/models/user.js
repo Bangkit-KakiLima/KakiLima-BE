f32105de86744b9e5c60e0a01d3affea35643523
Diekasptn <diekaseptiani7@gmail.com>
2024-11-25 05:57:12 +0000

    conflict: TODO fix version conflict This change needed because models must load use sequelize method

--- database/models/user.js
+++ database/models/user.js
@@ -1,61 +1,57 @@
 "use strict";
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/db.config"); // pastikan file ini sesuai
 
-class User extends Model {
-  /**
-   * Helper method for defining associations.
-   * This method is not a part of Sequelize lifecycle.
-   * The `models/index` file will call this method automatically.
-   */
-  static associate(models) {
-    // Define association here (if necessary)
-    // Example: this.hasMany(models.Post);
+module.exports = (sequelize) => {
+  class User extends Model {
+    static associate(models) {
+      // Define associations here
+      // Example: this.hasMany(models.Post);
+    }
   }
-}
 
-User.init(
-  {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      otp_code: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      otp_expiration: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      is_verified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.ENUM("seller", "buyer"),
+        allowNull: false,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    otp_code: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    otp_expiration: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    is_verified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    role: {
-      type: DataTypes.ENUM("seller", "buyer"),
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "User",
-    tableName: "Users",
-    timestamps: true,
-  }
-);
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "Users",
+      timestamps: true,
+    }
+  );
 
-module.exports = User;
+  return User;
+};
