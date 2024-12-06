3bdcb5fef07611a501294cd50bd3c917a7b54b5d
fadrahmangama <fadrahmangama@student.telkomuniversity.ac.id>
2024-12-06 10:47:56 +0700

    Feat: seeding, env, productCategoryFilter

--- database/seeders/20241124194631-demo-merchants.js
+++ database/seeders/20241124194631-demo-merchants.js
@@ -1,39 +1,416 @@
 "use strict";
-const { v4: uuidv4 } = require("uuid");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const merchants = [
-      { name: "Sate Taichan Lezat", rating: 4.5 },
-      { name: "Warung Makan Paijo", rating: 4.2 },
-      { name: "Kedai Kopi Sore", rating: 4.8 },
-      { name: "Pizza Galore", rating: 4.7 },
-      { name: "Toko Roti Manis", rating: 4.3 },
-      { name: "Ayam Goreng Sederhana", rating: 4.1 },
-      { name: "Roti Bakar Klasik", rating: 4.4 },
-      { name: "Mie Goreng Khas Paijo", rating: 4.0 },
-      { name: "Dapoer Cinde", rating: 4.3 },
-      { name: "Jus Buah Sehat", rating: 4.6 },
-      { name: "Kue Cubir Bandung", rating: 4.2 },
-      { name: "Soto Ayam Pak Gani", rating: 4.1 },
-      { name: "Pisang Bakar Sambel", rating: 4.5 },
-      { name: "Kedai Makan Keluarga", rating: 4.0 },
-      { name: "Tahu Tempe Kriuk", rating: 4.6 },
-      { name: "Nasi Goreng Surya", rating: 4.2 },
-      { name: "Es Krim Coklat Manis", rating: 4.7 },
-      { name: "Bakso Malang Mantap", rating: 4.4 },
-      { name: "Taco Corner", rating: 4.1 },
-      { name: "Martabak Ayam Bakar", rating: 4.3 },
-      { name: "Dimsum Sehat", rating: 4.6 },
-      { name: "Sate Kambing Pak Yanto", rating: 4.2 },
-      { name: "Kopi Dolo", rating: 4.8 },
-      { name: "Nasi Padang Alim", rating: 4.5 },
-      { name: "Kebap House", rating: 4.0 },
-      { name: "Cemilan Suka-Suka", rating: 4.2 },
-      { name: "Seafood Asli", rating: 4.7 },
-      { name: "Kedai Nasi Uduk", rating: 4.3 },
-      { name: "Ayam Penyet Bumbu", rating: 4.6 },
-      { name: "Sop Buntut Hasyim", rating: 4.0 },
+      {
+        name: "Warung Nasi Goreng Spesial",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Depot Mie Goreng Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Mie Lendir Maknyus",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bakso Malang Mantap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Batagor Bandung Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Cendol Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Teler Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Soto Bandoeng Sedap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bakso Tahu Si Om",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Mie Kocok Bandung Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bandrek Hangat Pak Dedi",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Kopi Susu Kekinian",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Serabi Bandung Mang Encep",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Pisang Molen Mbah Min",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Lapis Legit Ciamik",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Kue Apem Simbok",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Rujak Fresh",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Salad Buah Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Asinan Buah Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Roti Bakar Malam",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Crepes Delight",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Seblak Seuhah Mantap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Telor Gulung Bang Udin",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Cireng Racing Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Doger Betawi",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Kelapa Murni",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Fried Chicken Crispy",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Cuanki Serayu Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bebek Bakar Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sop Iga Sapi Sedap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sate Madura Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Nasi Uduk Kampung",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Tebu Murni",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Jus Segar Bu Broto",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Risol Mayo Renyah",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Gehu Pedas Mantap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Pisang Crispy Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Odading Gembul",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Dadar Gulung Simbah",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Kue Sus Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Donat Mini Manis",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Durian Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Mie Ayam Favorit",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Cuanki Serayu Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bebek Bakar Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sop Iga Sapi Sedap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sate Madura Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Nasi Uduk Kampung",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Tebu Murni",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Semangka Potong Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Kulkul Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Kacang Rebus Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Teh Mantul",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bubur Ayam Hangat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Tahu Gejrot Pak Slamet",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Makaroni Basah Spesial",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Roti Canai Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Campur Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Pempek Palembang Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Ayam Penyet Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sate Padang Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Tahu Bulat Garing",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Krim Mochi Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Nasi Liwet Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Cendol Dawet Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Ayam Geprek Mantap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sop Buntut Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Nasi Kuning Favorit",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Ketoprak Asli Betawi",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Rendang Padang Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Cincau Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Pisang Goreng Manis",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sop Buntut Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Nasi Kuning Favorit",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Ketoprak Asli Betawi",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Lontong Sayur Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Tahu Sumedang Spesial",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Rawon Jawa Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Martabak Manis Spesial",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Martabak Telur Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Serabi Bandung Mang Encep",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Pisang Molen Mbah Min",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Lapis Legit Ciamik",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Kue Apem Simbok",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Jeruk Nipis Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Dimsum Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Susu Jahe Hangat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bakwan Jagung Gurih",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Serabi Bandung Favorit",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Bakso Malang Sedap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Es Doger Spesial",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Rendang Padang Asli",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Soto Betawi Mantap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Nasi Goreng Kampung",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Capcay Kuah Sedap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Mie Aceh Juara",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Ikan Bakar Jimbaran",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Karedok Segar",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Sayur Lodeh Mantap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Pisang Goreng Keju Lezat",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Coto Makassar Enak",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
+      {
+        name: "Ayam Taliwang Sedap",
+        rating: parseFloat((Math.random() * 5).toFixed(1)),
+      },
     ];
 
     const getRandomTime = (start, end) => {
--- database/seeders/20241201175822-product_category_mappings.js
+++ database/seeders/20241201175822-product_category_mappings.js
@@ -55,6 +55,568 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        product_id: 8,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 9,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 10,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 11,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 12,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 13,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 14,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 15,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 16,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 17,
+        category_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 18,
+        category_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 19,
+        category_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 20,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 21,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 22,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 23,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 24,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 25,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 26,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 27,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 28,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 29,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 30,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 31,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 32,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 33,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 34,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 35,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 36,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 37,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 38,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 39,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 40,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 41,
+        category_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 42,
+        category_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 43,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 44,
+        category_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 45,
+        category_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 46,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 47,
+        category_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 48,
+        category_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 49,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 50,
+        category_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+    //new data from aeg
+  
+    {
+      product_id: 51,
+      category_id: 4, // Kue
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 52,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 53,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 54,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 55,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 56,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 57,
+      category_id: 4, // Kue
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 58,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 59,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 60,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 61,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 62,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 63,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 64,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 65,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 66,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 67,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 68,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 69,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 70,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 71,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 72,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 73,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 74,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 75,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 76,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 77,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 78,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 79,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 80,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 81,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 82,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 83,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 84,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 85,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 86,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 87,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 88,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 89,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 90,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 91,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 92,
+      category_id: 3, // Cemilan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 93,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 94,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 95,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 96,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 97,
+      category_id: 2, // Minuman
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 98,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 99,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      product_id: 100,
+      category_id: 1, // Makanan
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+
     ]);
   },
 
