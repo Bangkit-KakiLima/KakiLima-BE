f18cb05c127e5a54010f64c3283e3ebee64878d0
fadrahmangama <fadrahmangama@student.telkomuniversity.ac.id>
2024-12-10 16:47:59 +0700

    Feat: ML Recommendation endpoint

--- src/modules/productModules/product.route.js
+++ src/modules/productModules/product.route.js
@@ -1,9 +1,15 @@
 const Controller = require("./product.controller");
 const Upload = require("./product.middleware");
 const express = require("express");
+const authMiddleware = require("../authModules/auth.middleware");
 const ProductRouter = express.Router();
 
 ProductRouter.post("/", Upload.single("image"), Controller.CreateProduct);
+ProductRouter.post(
+  "/ml/recommendations",
+  authMiddleware,
+  Controller.MachineLearningRecommendationController
+);
 ProductRouter.get("/", Controller.GetAllProducts);
 ProductRouter.get("/recommendations", Controller.GetRecommendationProduct);
 ProductRouter.get(
